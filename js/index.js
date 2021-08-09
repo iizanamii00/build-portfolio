df4a7547c6db6d142fdcae8b0c5db3ebf0bb7706
iizanamii00 <naxatravasaani23@gmail.com>
2021-08-09 23:52:31 +0530

    added collapse on leave to sidebar

--- js/index.js
+++ js/index.js
@@ -29,3 +29,9 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 } 
+
+const sidebar = document.querySelector('.navbar');
+
+sidebar.addEventListener('mouseleave', () => {
+    document.body.classList.remove('nav-open');
+});
